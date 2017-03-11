02d48d65ce45d10d474639ce819ea328e4a75c02
Mitsunori Komatsu <komamitsu@gmail.com>
2017-03-11 18:15:44 +0900

    Add `setq`

--- src/eval.rs
+++ src/eval.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use parser::Node;
 
 struct Eval;
@@ -7,7 +8,7 @@ impl Eval {
         Eval {}
     }
 
-    fn calc_integer<F>(&self, f: &F, args: &[Node], node: &Node) -> Node
+    fn calc_integer<F>(&self, env: &mut HashMap<String, Node>, f: &F, args: &[Node], node: &Node) -> Node
         where F: Fn(i64, i64) -> i64 {
 
         Node::Integer(
@@ -17,7 +18,15 @@ impl Eval {
                     None => Some(i)
                 },
                 // TODO: Don't copy
-                &Node::List(_) => match &self.eval(x.clone()) {
+                &Node::List(_) => match &self.eval(env, x.clone()) {
+                    &Node::Integer(i) => match a {
+                        Some(aa) => Some(f(aa, i)),
+                        None => Some(i)
+                    },
+                    _ => panic!("{:?} takes only an integer, but got {:?}", node, x),
+                },
+                // TODO: Don't copy
+                &Node::Keyword(_) => match &self.eval(env, x.clone()) {
                     &Node::Integer(i) => match a {
                         Some(aa) => Some(f(aa, i)),
                         None => Some(i)
@@ -29,7 +38,7 @@ impl Eval {
         ).unwrap_or(0))
     }
 
-    fn car(&self, args: &[Node], node: &Node) -> Node {
+    fn car(&self, env: &mut HashMap<String, Node>, args: &[Node], node: &Node) -> Node {
         if args.len() == 1 {
             if let &Node::QuotedList(ref xs) = &args[0] {
                 return match xs.split_first() {
@@ -43,7 +52,7 @@ impl Eval {
         panic!("`car` takes only a quoted list, but got {:?}", args);
     }
 
-    fn cdr(&self, args: &[Node], node: &Node) -> Node {
+    fn cdr(&self, env: &mut HashMap<String, Node>, args: &[Node], node: &Node) -> Node {
         if args.len() == 1 {
             if let &Node::QuotedList(ref xs) = &args[0] {
                 return match xs.split_first() {
@@ -54,25 +63,59 @@ impl Eval {
             }
         }
 
-        panic!("`car` takes only a quoted list, but got {:?}", args);
+        panic!("`cdr` takes only a quoted list, but got {:?}", args);
     }
 
-    pub fn eval(&self, node: Node) -> Node {
+    fn setq(&self, env: &mut HashMap<String, Node>, args: &[Node], node: &Node) -> Node {
+        if args.len() % 2 == 0 {
+            let mut key = None;
+            for arg in args {
+                if let Some(k) = key {
+                    // TODO: Don't copy
+                    env.insert(k, arg.clone());
+                    key = None;
+                }
+                else {
+                    if let &Node::Keyword(ref k) = arg {
+                        // TODO: Don't copy
+                        key = Some(k.clone())
+                    }
+                    else {
+                        panic!("`setq` accepts only Node::Keyword as a key, but got {:?}", arg);
+                    }
+                }
+            }
+        }
+
+        panic!("`setq` takes only key value pairs, but got {:?}", args);
+    }
+    pub fn eval(&self, env: &mut HashMap<String, Node>, node: Node) -> Node {
         match node {
             Node::Integer(_) => node,
-            Node::Keyword(_) => panic!("Unexpected node: {:?}", node),
+            Node::Keyword(kwd) => {
+                // TODO: Don't copy
+                let cloned_env = env.clone();
+                let x = cloned_env.get(&kwd).unwrap();
+                self.eval(env, x.clone())
+            }
             Node::List(ref xs) => {
                 let (hd, tl) = xs.split_first().unwrap();
                 match hd {
                     &Node::Keyword(ref kwd) => 
                         match kwd.as_str() {
-                            "+" => self.calc_integer(&|a, i| a + i, tl, &node),
-                            "-" => self.calc_integer(&|a, i| a - i, tl, &node),
-                            "*" => self.calc_integer(&|a, i| a * i, tl, &node),
-                            "/" => self.calc_integer(&|a, i| a / i, tl, &node),
-                            "car" => self.car(tl, &node),
-                            "cdr" => self.cdr(tl, &node),
-                            _ => panic!("Unexpected node: {:?}", node),
+                            "+" => self.calc_integer(env, &|a, i| a + i, tl, &node),
+                            "-" => self.calc_integer(env, &|a, i| a - i, tl, &node),
+                            "*" => self.calc_integer(env, &|a, i| a * i, tl, &node),
+                            "/" => self.calc_integer(env, &|a, i| a / i, tl, &node),
+                            "car" => self.car(env, tl, &node),
+                            "cdr" => self.cdr(env, tl, &node),
+                            "setq" => self.setq(env, tl, &node),
+                            kwd => {
+                                // TODO: Don't copy
+                                let cloned_env = env.clone();
+                                let x = cloned_env.get(kwd).unwrap();
+                                self.eval(env, x.clone())
+                            },
                         },
                     _ => panic!("Unexpected node: {:?}", node),
                 }
@@ -84,6 +127,7 @@ impl Eval {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use super::Eval;
     use parser::Node;
 
@@ -92,6 +136,7 @@ mod tests {
         assert_eq!(
             Node::Integer(42),
             Eval::new().eval(
+                &mut HashMap::new(),
                 Node::List(
                     vec![
                         Node::Keyword(String::from("+")),
@@ -105,6 +150,7 @@ mod tests {
         assert_eq!(
             Node::Integer(42),
             Eval::new().eval(
+                &mut HashMap::new(),
                 Node::List(
                     vec![
                         Node::Keyword(String::from("*")),
@@ -124,6 +170,7 @@ mod tests {
         assert_eq!(
             Node::Integer(42),
             Eval::new().eval(
+                &mut HashMap::new(),
                 Node::List(
                     vec![
                         Node::Keyword(String::from("car")),
@@ -147,6 +194,7 @@ mod tests {
                 ]
             ),
             Eval::new().eval(
+                &mut HashMap::new(),
                 Node::List(
                     vec![
                         Node::Keyword(String::from("cdr")),
@@ -157,6 +205,22 @@ mod tests {
                                 Node::Integer(2),
                             ]
                         ),
+                    ]
+                )
+            )
+        );
+
+        let env = &mut HashMap::new();
+        env.insert(String::from("x"), Node::Integer(40));
+        assert_eq!(
+            Node::Integer(42),
+            Eval::new().eval(
+                env,
+                Node::List(
+                    vec![
+                        Node::Keyword(String::from("+")),
+                        Node::Integer(2),
+                        Node::Keyword(String::from("x"))
                     ]
                 )
             )
